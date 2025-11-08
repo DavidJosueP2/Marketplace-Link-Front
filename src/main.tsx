35fcf8183f1bbc2f76c71556286c7c0ef7672f92
DRTX2 <davidmanjarres2004@gmail.com>
2025-11-08 18:03:51 -0500

    feat(frontend): update main.tsx for API environment variable support

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import AppProviders from "@/inc/providers/AppProviders";
 import "@/index.css";
+import "@/styles/decor.css";
 
 const root = document.getElementById("root") as HTMLElement;
 

e575a72d274632db6875170b94f7d34659c2c8e3
DRTX2 <davidmanjarres2004@gmail.com>
2025-12-02 15:51:12 -0500

    edit config

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // Runtime configuration - Se puede sobrescribir con variables de entorno en Azure
 window.ENV = {
-  VITE_API_URL: 'VITE_API_URL_PLACEHOLDER'
+  VITE_API_URL: 'https://mplink-backend.purplebay-4e22b9c6.westus3.azurecontainerapps.io'
 };
